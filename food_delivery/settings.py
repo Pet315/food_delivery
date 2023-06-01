51f09cdfb3db19604fb86d5b96228eb4c4a08dd3
Pet315 <oppapeter17@gmail.com>
2023-06-01 18:38:45 +0300

    Add changes

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import secret_key
 import django_heroku
 import dj_database_url
 import os
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key.sk
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
