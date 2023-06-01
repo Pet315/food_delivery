8629cc0effe401d210874618cf5ac8966b9afaef
Pet315 <oppapeter17@gmail.com>
2023-06-01 19:39:00 +0300

    Add changes

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from main.models import *
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Shop)
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(Order)
+
+class ShopAdmin(ImportExportModelAdmin):
+    pass
+
+
+class ProductAdmin(ImportExportModelAdmin):
+    pass
+
+
+class CartAdmin(ImportExportModelAdmin):
+    pass
+
+
+class OrderAdmin(ImportExportModelAdmin):
+    pass
+
+
+admin.site.register(Shop, ShopAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
